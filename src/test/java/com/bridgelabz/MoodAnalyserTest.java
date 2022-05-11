95fced1e7a150becab58f523bdce59ec2033ef2c
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-12 01:16:30 +0530

    Added:Welcomne message is added

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Mood analyzer problem :-) ");
+    }
+}
